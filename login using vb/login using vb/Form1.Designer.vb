4a97881fda2a465d8185b7bbd1f9116d535e5932
iamlogeshwaran <113006185+iamlogeshwaran@users.noreply.github.com>
2023-02-17 22:13:21 +0530

    Add files via upload

---
+++ login using vb/login using vb/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Txtname = New System.Windows.Forms.TextBox()
+        Me.Txtpassword = New System.Windows.Forms.TextBox()
+        Me.login = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 106)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(44, 164)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password"
+        '
+        'Txtname
+        '
+        Me.Txtname.Location = New System.Drawing.Point(121, 106)
+        Me.Txtname.Name = "Txtname"
+        Me.Txtname.Size = New System.Drawing.Size(100, 20)
+        Me.Txtname.TabIndex = 2
+        '
+        'Txtpassword
+        '
+        Me.Txtpassword.Location = New System.Drawing.Point(121, 161)
+        Me.Txtpassword.Name = "Txtpassword"
+        Me.Txtpassword.Size = New System.Drawing.Size(100, 20)
+        Me.Txtpassword.TabIndex = 3
+        '
+        'login
+        '
+        Me.login.Location = New System.Drawing.Point(133, 210)
+        Me.login.Name = "login"
+        Me.login.Size = New System.Drawing.Size(75, 23)
+        Me.login.TabIndex = 4
+        Me.login.Text = "Login"
+        Me.login.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 320)
+        Me.Controls.Add(Me.login)
+        Me.Controls.Add(Me.Txtpassword)
+        Me.Controls.Add(Me.Txtname)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Txtname As System.Windows.Forms.TextBox
+    Friend WithEvents Txtpassword As System.Windows.Forms.TextBox
+    Friend WithEvents login As System.Windows.Forms.Button
+
+End Class
---
+++ login using vb/login using vb/Form1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Class Form1
+
+    Private Sub login_Click(sender As Object, e As EventArgs) Handles login.Click
+        Dim con As SqlConnection = New SqlConnection("SERVER=.\SQLEXPRESS; Database=Restaurant;integrated security=true")
+        Dim cmd As SqlCommand = New SqlCommand("select passcode from manager where name='" + Txtname.Text + "'")
+        Dim dbpass As String
+        con.Open()
+        cmd.Connection = con
+        dbpass = cmd.ExecuteScalar()
+        Dim password As String
+        password = Txtpassword.Text
+        If (password.Equals(dbpass)) Then
+            MessageBox.Show("login success", "information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("Error", "information", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        con.Close()
+        End If
+    End Sub
+End Class
